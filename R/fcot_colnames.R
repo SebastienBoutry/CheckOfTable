83e3f8d853a7e6d317c2c50e91de9816ae00704f
Boutry <sebastien.boutry@inrae.fr>
2021-12-24 16:50:42 +0100

    modification des packages tidyverse par dplyr, tibblee tidyr

--- R/fcot_colnames.R
+++ R/fcot_colnames.R
@@ -17,16 +17,16 @@
 #' fcot_colnames(obj,noms_colonnes=c("a"),table_orig="table test")
 fcot_colnames <- function(data,noms_colonnes,table_orig){
   ##
-  test <- f_format_table(data,table_orig)
+  test <- CheckOfTable::fcot_table(data,table_orig)
   ##
   data <- data %>%
     # rename_with(str_to_upper) %>%
-    rename_with(str_trim)
+    dplyr::rename_with(str_trim)
   ##
   if(test$valeur_test == "oui"){
     verif <- which(! (noms_colonnes %in% names(data)))
     if(length(verif)==0){
-      test <- tibble(test="1.2",
+      test <- tibble::tibble(test="1.2",
                      valeur_test="oui",
                      nom_test="data.frame_colnames",
                      table_orig=table_orig,
@@ -36,7 +36,7 @@ fcot_colnames <- function(data,noms_colonnes,table_orig){
                      message="")
       # )
     }else{
-      test <- tibble(test="1.2",
+      test <- tibble::tibble(test="1.2",
                      valeur_test="non",
                      nom_test="data.frame_colnames",
                      table_orig=table_orig,
@@ -51,7 +51,7 @@ fcot_colnames <- function(data,noms_colonnes,table_orig){
     }
 
   }else{
-    test <- tibble(test="1.2",
+    test <- tibble::tibble(test="1.2",
                    valeur_test="non",
                    nom_test="colnames_data.frame",
                    table_orig=table_orig,
