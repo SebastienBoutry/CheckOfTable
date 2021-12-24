0ba287d6ee1fb13b00f850562e5b48ab9ec93551
Boutry <sebastien.boutry@inrae.fr>
2021-12-24 08:22:04 +0100

    intégration de la fonction fcot_value_empty

---
+++ R/fcot_value_empty.R
@@ -0,0 +1,51 @@
+fcot_value_empty <- function(data,noms_colonnes,table_orig){
+  ##
+  test <- NULL
+  ##
+  verif <- sapply(noms_colonnes,
+                  function(i)
+                    (
+                      sum(!(data %>% pull(i) %in% c(NA,""))) == nrow(data)
+                    )
+  )
+  ##
+  if(sum(verif) == length(noms_colonnes)){
+    test <- tibble(test="2.2",
+                   valeur_test="oui",
+                   nom_test="colnames_value",
+                   table_orig=table_orig,
+                   # sortie=data.frame(
+                   test_precis="2.2.1",
+                   nom_test_precis="colnames_value_empty",
+                   # test="format_data.frame",
+                   message=paste0("")
+                   # )
+    )
+  }else{
+    for(i in noms_colonnes[verif==FALSE]){
+      verif <- which(
+        (data %>% pull(i) %in% c(NA,""))
+      )
+
+      ##
+      test <- bind_rows(test,
+                        tibble(test="2.2",
+                               valeur_test="non",
+                               nom_test="colnames_value",
+                               table_orig=table_orig,
+                               # sortie=data.frame(
+                               test_precis=paste0("2.2.1_",i),
+                               # test="format_data.frame",
+                               nom_test_precis="colnames_value_empty",
+                               message=paste0("La variable suivante ",
+                                              i,
+                                              " possède ",
+                                              length(verif),
+                                              " valeur(s) vide(s).\n Lignes :",
+                                              paste0(verif,collapse = ", "))
+                        )
+      )
+    }
+  }
+  return(test)
+}
